07ed2454e2dfdd3ce39acef12e37522533f5f794
Will Blain <willblain@Mac.gateway.sasktel.net>
2025-08-07 20:18:31 -0600

    Initial hello program

---
+++ hello.py
@@ -0,0 +1,9 @@
+print("-----------------")
+name = input("enter your name: ")
+print("hello " + name)
+check = input("are you sure thats your name?")
+if check == "yes":
+    print("ok")
+else: name = input("what is your name then? ")
+print("hello " + name)
+print("-----------------")
